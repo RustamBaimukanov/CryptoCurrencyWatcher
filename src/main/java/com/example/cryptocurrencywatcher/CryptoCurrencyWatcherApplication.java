b61d865a0a7ee81af33f2fca192f5874e63bca9d
RustamBaimukanov <watashirustam@gmail.com>
2022-01-26 17:54:02 +0300

    Инициализация

---
+++ src/main/java/com/example/cryptocurrencywatcher/CryptoCurrencyWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrencywatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCurrencyWatcherApplication.class, args);
+    }
+
+}
